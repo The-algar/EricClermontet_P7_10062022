1efb05fe3a56846fcc108a174244c3e94b55618a
ERIC CLERMONTET <emc@go4.social>
2022-07-21 16:24:25 +0200

    Ingredient filter Search WIP

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,62 +2,104 @@
 import RecipeSearch from "../search/search.js";
 import RecipeFactory from "../factories/RecipeFactory.js";
 
-/* Récupère tableau recipe via fetch, filtre le tableau en fonction saisie du champ de recherche recette et affiche les cartes recettes */
+let filteredRecipes = [];
+
+/* Récupère tableau recipe via fetch */
 async function init() {
-   const indexSectionRecette = document.querySelector(".sectionRecettes");
-   const indexSearch = document.querySelector("#mainSearch");
+
+   let indexSectionRecette = document.querySelector(".sectionRecettes");
+   const mainSearch = document.querySelector("#globalSearch");
    const recipeSearch = new RecipeSearch();
 
-   /* Methode fetchData: récupération data + ajout propriété recipeSearch: array 50 instances recette */
-   recipeSearch.fetchData().then(() => {
-      console.log(recipeSearch);
 
-      // EventListener sur <input> champ de recherche recette
-      indexSearch.addEventListener("input", (event) => {
+   /*************
+   *************    FETCH     *******************
+   *************/
 
-         /* Méthode indexSearch filtre tableau instance recette en fonction saisie input */
-         const filteredRecipes = recipeSearch.indexSearch(event.target.value);
-         console.log(filteredRecipes);
+   /* récupération data + ajout propriété recipeSearch: array 50 instances recette */
+   await recipeSearch.fetchData();
 
-         // Supression des recettes préexistantes à la nouvelle saisie
-         while (indexSectionRecette.firstChild) {
-            indexSectionRecette.removeChild(indexSectionRecette.firstChild)
-         }
-         // Affichage du html des nouvelles recettes
-         filteredRecipes.forEach((instRecipe) => {
-            const recipeFactory = new RecipeFactory(instRecipe);
-            indexSectionRecette.appendChild(recipeFactory.createRecipeCards());
-         })
+
+   /*************
+   *************    FILTRES     *******************
+   *************/
+
+   const inputIngredient = document.querySelector("#searchIngredient");
+
+
+   /* Ajout liste ingrédients dans filtre */
+   createIngredientList(recipeSearch.getIngredients(null, ));
+
+   /* Listener champ recherche filtre */
+   inputIngredient.addEventListener("change", (event) => {
+      const saisie = event.target.value;
+      createIngredientList(recipeSearch.getIngredients(null, saisie));
+
+   })
+
+
+   /************* Menu Dropdown *************/
+   const ingredientFilter = document.querySelector("#ingredientFilter");
+   const nodeIconFilter = document.querySelector(".filter img");
+   const ingredientUl = document.querySelector("#ingredientList");
+
+   inputIngredient.addEventListener("click", (event) => {
+      if (!ingredientUl.classList.contains("appear")) {
+         /* Modification <input type="text" -> "search" */
+         ingredientUl.classList.add("appear");
+         ingredientFilter.classList.add("unsetFilter");
+         event.target.setAttribute("type", "search");
+         event.target.setAttribute("type", "search");
+         event.target.removeAttribute("value");
+         event.target.setAttribute("placeholder", "Rechercher un ingredient");
+         nodeIconFilter.classList.add("rotate");
+
+      } else {
+         ingredientUl.classList.remove("appear");
+         ingredientFilter.classList.remove("unsetFilter")
+         event.target.setAttribute("type", "button");
+         event.target.removeAttribute("placeholder");
+         event.target.setAttribute("value", "ingredient");
+         nodeIconFilter.classList.remove("rotate");
+      }
+   })
+
+
+   /*************
+   *************    Recherche globale     *******************
+   *************/
+
+   // EventListener sur <input> champ de recherche recette
+   mainSearch.addEventListener("input", (event) => {
+      /* Méthode rechercheGlobale filtre tableau instance recette en fonction saisie input */
+      filteredRecipes = recipeSearch.globalSearch(event.target.value);
+      // Supression des recettes préexistantes à la nouvelle saisie
+      indexSectionRecette.innerHTML = null;
+      // Affichage du html des nouvelles recettes
+      filteredRecipes.forEach((instRecipe) => {
+         const recipeFactory = new RecipeFactory(instRecipe);
+         indexSectionRecette.appendChild(recipeFactory.createRecipeCards());
       })
+   })
 
-      // Dropdown bouton ustensile
-      const nodeFilter = document.querySelector(".filter");
-      const contFilterOpen = document.querySelector(".contFilterOpen");
-      const nodeNameFilter = document.querySelector(".filter span");
-      const nodeSearchFilter = document.querySelector("#filterSearch");
-      const nodeIconFilter = document.querySelector(".filter img");
 
-      nodeFilter.addEventListener("click", () => {
-         if (!contFilterOpen.classList.contains("appear")) {
-            contFilterOpen.classList.add("appear");
-            nodeNameFilter.classList.add("disappear");
-            nodeFilter.classList.add("unsetFilter");
-            nodeIconFilter.classList.add("rotate");
-            nodeSearchFilter.focus();
 
-         }else {
-            contFilterOpen.classList.remove("appear");
-            nodeFilter.classList.remove("unsetFilter");
-            nodeNameFilter.classList.remove("disappear")
-            nodeIconFilter.classList.remove("rotate");
-         }
-      })
 
-      // Recherche filtres
-      nodeSearchFilter.addEventListener("input", (event) => {
-        const filteredIngredient = recipeSearch.ingredientSearch(event.target.value);
-        console.log(filteredIngredient);
-        
+
+
+
+}
+init();
+
+      function createIngredientList(ingredientsList) {
+         const ingredientUl = document.querySelector("#ingredientList");
+         // Supression des listes existantes
+         ingredientUl.innerHTML = null;
+         // Ajout des nouvelles listes
+         ingredientsList.forEach((el) => {
+            const list = document.createElement("li");
+            list.innerHTML = el;
+            ingredientUl.appendChild(list);
         
 
       })
@@ -72,8 +114,4 @@ async function init() {
 
       
 
-   })
-
-
-}
-init();
+   }
--- scripts/search/search.js
+++ scripts/search/search.js
@@ -4,7 +4,7 @@ export default class RecipeSearch {
     constructor() {
         this.recipes = [];
     }
-    indexSearch(entry) {
+    globalSearch(entry) {
         const entryLow = entry.toLowerCase();
         const arrayRecipeFiltered = [];
         this.recipes.forEach((instRecipe) => {
@@ -46,23 +46,40 @@ export default class RecipeSearch {
             })
     }
 
-    ingredientSearch(entry){
-    const finder = this.recipes.filter((ingreRecipe)=> {
-        let foundRecipe = false;
-        // ingedredient found in table ingredients
-        ingreRecipe.ingredients.forEach((objIngredient) => {
-            if (objIngredient.ingredient.includes(entry)) {
-                foundRecipe = true;
-            } 
-        })
-        return foundRecipe;
-    });
-    console.log(finder);
-    return finder
-    }
+    // Extrait liste ingrédients du tableau recette complet ou filtré
+    // Filtre2 avec search <input>
+    getIngredients(filteredRecipes, searchIngredient) {
 
+        // Transformation array d'objet recette -> array de liste d'ingrédients
+        // map sur filteredRecipes si existe, sinon sur tableau recettes non modifié
+        let listIngredients = (filteredRecipes || this.recipes).map((objRecette) => {
+            return objRecette.ingredients.map((objIngredient) => {
+                return objIngredient.ingredient.toLowerCase()});
+           });
+       // Array d'array liste -> array string liste, supprime 1 imbrication    
+       listIngredients = listIngredients.flat();
+       // Obj Set -> supprime doublons, spread [... set] conversion set -> array
+       listIngredients = [... new Set(listIngredients)];
 
-
-
-
+       if (searchIngredient) {
+           listIngredients = listIngredients.filter((saisie) => saisie.indexOf(searchIngredient.toLowerCase()) > -1);
+       }
+       return listIngredients
+   }
 }
+
+/*
+   getIngredients(filteredRecipes, searchIngredient) {
+       let ingredients = [
+           ...new Set((filteredRecipes || this.recipes).map((recipe) => {
+               return recipe.ingredients.map((ingredient) => ingredient.ingredient.toLowerCase())
+           }).flat())
+       ];
+       if (searchIngredient) {
+           ingredients = ingredients.filter((ingredient) => ingredient.indexOf(searchIngredient.toLowerCase()) > -1);
+       }
+       console.log(ingredients);
+       return ingredients
+   }
+   
+*/
